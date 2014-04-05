d5d683c4ed31059546d0c0938f6a88d1e53b43dd
Tadanori Suyama <tadanori@Mint-Dell.(none)>
2014-04-05 12:02:18 +0900

    Add COBOL Source

---
+++ COBOL/hello.cob
@@ -0,0 +1,8 @@
+123456*8901234567890123456789012345678901234567890
+      * hello.cob
+       IDENTIFICATION  DIVISION.
+       PROGRAM-ID.     HELLO.
+       PROCEDURE       DIVISION.
+           DISPLAY "Hello, World!".
+           STOP   RUN.
+
